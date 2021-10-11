0aeb100daef17527e8645fe3498e85e8cf098918
SanderKirchert <Sanders@post.cybercity.dk>
2021-10-11 20:57:00 +0200

    WTF!

--- DNPAssignment1/Models/Person.cs
+++ DNPAssignment1/Models/Person.cs
@@ -5,13 +5,22 @@ namespace Models
     public class Person
     {
         public int Id { get; set; }
+        
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string HairColor { get; set; }
+        [Required]
         public string EyeColor { get; set; }
+        [Required]
         public int Age { get; set; }
+        
         public float Weight { get; set; }
+        [Required]
         public int Height { get; set; }
+      
         public string Sex { get; set; }
         
     }
